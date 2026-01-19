bdd759a5e20402944c2fc2bdafa5f0bf5ecd84ba
lewisRT-tech <lewisrthomas207@gmail.com>
2026-01-19 17:25:02 +0000

    Fixed the debugger to work with the PIR sensor

--- Debug/PIRDebug.py
+++ Debug/PIRDebug.py
@@ -1,24 +1,21 @@
 import time
+import RPi.GPIO as GPIO # RPI.GPIO is only accessable on Rasberry PI
 
-try:
-    import RPI.GPIO as GPIO # RPI.GPIO is only accessable on Rasberry PI
-except:
-    raise ("RPI GPIO does not exist as a library due to an issue")
+MotionSensor = 17
 
-try:
-    GPIO.setmode(GPIO.BCM)
-except:
-    raise ("GPIO.setmode(GPIO.BCM) is unable to execute")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)
 
+
+print ("PIR Module Test (CTRL+C to exit)")
+time.sleep(3)
 try:
-    print ("PIR Module Test (CTRL+C to exit)")
-    time.sleep(3)
     while True:
-        if GPIO.input(PIR_PIN):
+        if GPIO.input(MotionSensor) == 1:
             print ("Motion detected")
             time.sleep(1)
         else:
             print ("No motion detected")
-except:
-    raise ("Error, program cannot accept PIR_PIN input")
-
+            time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()

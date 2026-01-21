1a0ca42d4ca9376afe2230319b3fd426b89a8a53
lewisRT-tech <lewisrthomas207@gmail.com>
2026-01-21 09:59:26 +0000

    Added buzzer debugging with the motion sensor

--- Debug/PIRDebug.py
+++ Debug/PIRDebug.py
@@ -2,9 +2,11 @@ import time
 import RPi.GPIO as GPIO # RPI.GPIO is only accessable on Rasberry PI
 
 MotionSensor = 17
+Alarm = 4
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
+GPIO.setup(MotionSensor, GPIO.IN)
+GPIO.setup(Alarm, GPIO.OUT)
 
 
 print ("PIR Module Test (CTRL+C to exit)")
@@ -13,7 +15,10 @@ try:
     while True:
         if GPIO.input(MotionSensor) == 1:
             print ("Motion detected")
+            GPIO.output(Alarm, GPIO.HIGH)
             time.sleep(1)
+            GPIO.output(Alarm, GPIO.LOW)
+            time.sleep(3)
         else:
             print ("No motion detected")
             time.sleep(1)
